139080339b060a767e4d70c938603843db102c05
PhantomOz <faniogor@gmail.com>
2024-04-07 09:28:17 +0100

    Created Board

---
+++ frontend/src/app/play/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+import Square from "../components/Square";
+import { useEffect, useState } from "react";
+
+export default function Arena() {
+    const [draggedElement, setDElement] = useState<EventTarget>();
+    const [turn, setTurn] = useState<boolean>(false);
+    const pieces = [
+        "rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook",
+        "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn",
+        "rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook",
+    ]
+    useEffect(() => {
+        if (draggedElement === undefined) {
+            setTurn(!turn);
+        }
+    }, [draggedElement]);
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+            <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+                <div className="flex flex-row w-[560px] gap-4 items-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:rounded-t-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                    <div className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"></div>
+                    <div>
+                        <p>0x123uifdriew9w9wq0q9wqw</p>
+                        <p>1443</p>
+                    </div>
+                    <div className="border-b ml-auto border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                        <p>10:00</p>
+                    </div>
+                </div>
+                <div className={`z-10 w-[560px] h-[560px] flex flex-row ${turn ? "flex-wrap" : "flex-wrap-reverse"}`}>
+                    {pieces.map((piece, index) => <Square key={index} piece={piece} index={index} setDElement={setDElement} draggedElement={draggedElement} />)}
+                </div>
+                <div className="flex flex-row w-[560px] gap-4 items-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:rounded-b-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                    <div className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"></div>
+                    <div>
+                        <p>0x123uifdriew9w9wq0q9wqw</p>
+                        <p>1443</p>
+                    </div>
+                    <div className="border-b ml-auto border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                        <p>10:00</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
